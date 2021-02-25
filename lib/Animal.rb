2612f525aa347dc60788ebdfe874e6f2e14f9275
Colten Appleby <colten.appleby@gmail.com>
2021-02-25 14:28:04 -0500

    created animal and zoo classe initialization methods

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,20 @@
 class Animal
 
+    attr_reader :nickname, :species
+    attr_accessor :weight
+
+    @@all_animals = [] #call variable
+
+
+    def initialize(species_arg, weight_arg, nickname_arg)
+        @species = species_arg
+        @weight = weight_arg
+        @nickname = nickname_arg
+        @@all_animals << self
+    end
+
+    def self.all  #class method
+        return @@all_animals
+    end
+
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,20 @@
 class Zoo
 
+    attr_reader :name, :location
+
+    @@all_zoos = []
+
+    def initialize(name_arg, location_arg)
+        @name = name_arg
+        @location = location_arg
+        @@all_zoos << self
+    end
+
+    def self.all
+        return @@all_zoos
+    end
+    
+
+
+
 end
